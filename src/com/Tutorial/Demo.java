f78d84d519d751a59a1afc1311b83863c308363e
Onkaresh Dighe <digheomkar11@gmail.com>
2022-04-25 06:52:55 +0530

    second commite

--- src/com/Tutorial/Demo.java
+++ src/com/Tutorial/Demo.java
@@ -1,5 +1,18 @@
 package com.Tutorial;
 
 public class Demo {
+	
 
-}
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") {
+			
+			System.out.println("eat Apple");
+			
+		}else {
+			
+			System.out.println("do some work first");
+		
+		}
+
+}}
